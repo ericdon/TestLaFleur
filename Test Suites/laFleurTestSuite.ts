3e6b51dc1255c7409e3c94ed63602d5178adddf2
DONDELINGER <edondelinger@gmail.com>
2018-10-31 15:18:27 +0100

    Testsuite leFleur

---
+++ Test Suites/laFleurTestSuite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>laFleurTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-10-31T14:45:43</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>b4f35861-c772-4182-aa24-b69aef153bfd</testSuiteGuid>
+   <testCaseLink>
+      <guid>f29f9dbd-0f26-4ac9-9255-617390507b01</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestPageAccueil</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
